7d9109d78ad940d1bac36ebe97c97869599a8af4
alamin-karno <amin15-1951@diu.edu.bd>
2023-02-07 12:22:11 +0600

    DocApp: Appoinment Booking API, Database & Contoller Added

---
+++ app/Http/Controllers/AppointmentsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointments;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AppointmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $appontments = new Appointments();
+        $appontments->user_id = Auth::user()->id;
+        $appontments->doc_id = $request->get('doc_id');
+        $appontments->date = $request->get('date');
+        $appontments->day = $request->get('day');
+        $appontments->time = $request->get('time');
+        $appontments->status = 'upcoming';
+        $appontments->save();
+
+        return response()->json([
+            'success'=>true, 
+            'message'=>'New Appoinment has been made successfully.',
+        ],200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Doctor;
+use App\Models\UserDetails;
+use App\Models\Appointments;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -66,5 +69,9 @@ class User extends Authenticatable
 
     public function user_details(){
         return $this->hasOne(UserDetails::class,'user_id');
+    }
+
+    public function appointments(){
+        return $this->hasMany(Appointments::class,'user_id');
     }
 }
